801c107d1bc8b2f8b042ab8907d8a5e2b5568665
fjscorrea <fjscorrea@gmail.com>
2018-05-16 15:00:39 -0300

    Aula 3 - Exercicio 2

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ COPY . /usr/src/
 WORKDIR /usr/src/
 RUN pip install -r requirements.txt
 ENV FLASK_APP=app.py
+EXPOSE 5000
 CMD ["flask","run","--host","0.0.0.0"]
 
--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-import logging
 from flask import Flask, request
 from logging.handlers import RotatingFileHandler
 
+from flask_redis import FlaskRedis
+
+import logging
+import datetime
+import os
+
+#Create the App
 app = Flask(__name__)
 
+server = os.getenv('REDIS_URL','redis')
+
+#Redis Connection URL
+app.config['REDIS_URL'] = "redis://{}:6379/0".format(server)
+
+#Bind Redis Connection to app
+redis_store = FlaskRedis(app)
+
+#Create logs
 handler = RotatingFileHandler('/tmp/foo.log', maxBytes=10000, backupCount=1)
 handler.setLevel(logging.INFO)
 app.logger.addHandler(handler)
 
+#Send Values to Redis.
+redis_store.set('Start Time', datetime.datetime.now())
+
 @app.route("/")
 def hello():
     app.logger.error(('The referrer was {}'.format(request.referrer)))
-    return "Hello World!"
+    return "Hello World! Cap 3 - Exercício 2"
